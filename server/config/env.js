8f0403a8b5eade3ee663c6ba38c2c929bdad1121
radelmann <robadelmann@gmail.com>
2016-03-14 20:12:51 -0700

    initialized environment vars for connecting to twitter api

--- server/config/env.js
+++ server/config/env.js
@@ -1,3 +1,11 @@
 module.exports = {
-  'port': process.env.TW_API_PROD_PORT || 3000
+  "port": process.env.TW_API_PROD_PORT || 3000,
+
+  "twitter": {
+    "consumerKey": process.env.TW_API_CONSUMER_KEY,
+    "consumerSecret": process.env.TW_API_CONSUMER_SECRET,
+    "accessToken": process.env.TW_API_ACCESS_TOKEN,
+    "accessTokenSecret": process.env.TW_API_ACCESS_TOKEN_SECRET,
+    "callBackUrl": process.env.TW_API_CALLBACK_URL
+  }
 };
