1f56a6df4a4faf442b58c4a787bb9d29628b242c
radelmann <robadelmann@gmail.com>
2016-03-15 19:41:36 -0700

    added toggle display photos functionality

--- client/app/tweets/tweets.js
+++ client/app/tweets/tweets.js
@@ -7,23 +7,26 @@ angular.module('twitterAPI.tweets', [])
     $scope.data.userDescription = '';
     $scope.data.userPhoto = '';
     $scope.data.screenName = '';
+    $scope.data.retweetFilter = 0;
+    $scope.data.displayPhotos = true;
 
     $scope.submitSearch = function() {
       tweets.get($scope.data.screenName).then(function(data) {
         console.dir(data);
 
-        //check for image in tweet
-        for (var i = 0; i < data.length; i++) {
-          if (data[i].entities.media) {
-            data[i].img = data[i].entities.media[0].media_url;
+        if (data.length > 0 && $scope.data.displayPhotos === true) {
+          //check for image in tweet
+          for (var i = 0; i < data.length; i++) {
+            if (data[i].entities.media) {
+              data[i].img = data[i].entities.media[0].media_url;
+            }
           }
         }
 
-        if (data.length > 0) {
-          $scope.data.userName = data[0].user.name;
-          $scope.data.userPhoto = data[0].user.profile_image_url;
-          $scope.data.userDescription = data[0].user.description;
-        }
+        $scope.data.userName = data[0].user.name;
+        $scope.data.userPhoto = data[0].user.profile_image_url;
+        $scope.data.userDescription = data[0].user.description;
+
         $scope.data.tweets = data;
       });
     };
